511541f57fdf6c1a8efb0e7015d532fdf205e689
karinasvatk <savitskayakarrina@outlook.com>
2025-12-05 09:57:03 +0000

    fix: correct onlyOwner modifier override in SubBaseV2

--- src/SubBaseV2.sol
+++ src/SubBaseV2.sol
@@ -11,7 +11,7 @@ import {AutomationModule} from "./modules/AutomationModule.sol";
  * @dev Adds ChargeModule and AutomationModule capabilities to V1
  */
 contract SubBaseV2 is SubBaseV1, AutomationModule {
-    modifier onlyOwner() override(SubBaseV1, ChargeModule) {
+    modifier onlyOwner() override {
         if (msg.sender != _owner) revert Unauthorized();
         _;
     }
